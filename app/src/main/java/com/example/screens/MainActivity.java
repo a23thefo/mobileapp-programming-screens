e096295922a780f078639912eca10ef3fc5a4bc5
Nixeff <theodore.folkesson@gmail.com>
2024-04-21 22:33:36 +0200

    Byte namn på MainActivity2 till ActivityTwo

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                Intent intent = new Intent(MainActivity.this, ActivityTwo.class);
                 intent.putExtra("msg", "Hej på dig!");
                 startActivity(intent);
             }
